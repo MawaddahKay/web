a1dda2505cfae3293237e66ce6f1b623df92560a
iiffah <Alifsaah136@gmail.com>
2022-09-26 11:52:58 +0700

    a

--- website/sell/views.py
+++ website/sell/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import *
+
+
 
 # Create your views here.
+
+
+
+def Index(request):
+    return render(request,'index.html')
+
+
