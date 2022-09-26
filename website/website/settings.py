a235e3c4b81d77c1b06695f1ffb75982eb0fdd29
iiffah <Alifsaah136@gmail.com>
2022-09-26 12:00:15 +0700

    b

--- website/website/settings.py
+++ website/website/settings.py
@@ -125,14 +125,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdm","static_root")
 
 
 
-MEDIA_ROOT= os.path.join(BASE_DIR,"media/")
-MEDIA_URL= "/media/"
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-LOGIN_URL = 'index'
-LOGOUT_REDIRECT_URL = 'login'
-LOGIN_REDIRECT_URL = 'index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
